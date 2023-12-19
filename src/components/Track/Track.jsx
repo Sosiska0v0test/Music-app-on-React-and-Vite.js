114d39a7a4e41d88f85f4282a3c0dadd0fee59c3
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2023-12-19 12:14:54 +0200

    Add files via upload

---
+++ src/components/Track/Track.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import { AudioContext } from '../../context/AudioContext';
+import style from './track.module.scss';
+import { IconButton } from '@mui/material';
+import { PlayArrow } from '@mui/icons-material';
+import secondsToMMSS from '../../utils/secondsToMMSS';
+
+const Track = track => {
+	const { id, src, preview, title, artists, duration } = track;
+
+	const { handleToggleAudio } = useContext(AudioContext);
+
+	const formattedDuration = secondsToMMSS(duration);
+
+	return (
+		<div className={style.track}>
+			<IconButton onClick={() => handleToggleAudio(track)}>
+				<PlayArrow />
+			</IconButton>
+			<img className={style.preview} src={preview} alt='' />
+			<div className={style.credits}>
+				<b>{title}</b>
+				<p>{artists}</p>
+			</div>
+			<p>{formattedDuration}</p>
+		</div>
+	);
+};
+
+export default Track;
